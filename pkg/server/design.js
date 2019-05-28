a1f5147cbe86d8724c71e96f598287340942a073
scipsycho <dsingh1664@outlook.com>
2019-05-28 08:42:12 +0530

    designed frontPage.html

---
+++ pkg/server/design.js
@@ -0,0 +1,29 @@
+@font-face {
+	font-family:"heading_font";
+	src: url("./heading_font.otf");
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+
+}
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+function getDisplayAll(text){
+  document.getElementById('displayAll').innerHTML = text
+
+}
+
+httpGetAsync("http://localhost:8080/record/displayAll", getDisplayAll)
